82337b1aee66a3e89d6311540ea8d79ff81923f3
Faisal Fahlevi <fhlevi2804@gmail.com>
2026-01-09 15:19:07 +0700

    Update database connection details in config.go

--- config/config.go
+++ config/config.go
@@ -24,11 +24,11 @@ func InitDB() {
     // Data Source Name (DSN) untuk koneksi ke database
     connStr := fmt.Sprintf(
         "host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-        "9qasp5v56q8ckkf5dc.apn.leapcellpool.com", 	// host
-        "6438",                                		// port
-        "kfuymccuwjqozuwmgzre",                		// user
-        "toyxnnujnbcrkmllrtviwmzqrfojiz",      		// password
-        "ulbcsxsmnrskzxjtmeyc",                		// dbname
+        "db.nhdpbrknskckotjybrrk.supabase.co", 	// host
+        "6543",                                		// port
+        "postgres",                		// user
+        "z0QhXN2Lew41ynBi",      		// password
+        "postgres",                		// dbname
         "require",                             		// sslmode
     )
 
